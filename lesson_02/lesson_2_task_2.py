f5e6713817cb01c8fdbc12521f34698cc5d13d3d
ladycaxapa <ladycaxapa@gmail.com>
2026-09-09 08:28:16 +0500

    correct

--- lesson_02/lesson_2_task_2.py
+++ lesson_02/lesson_2_task_2.py
@@ -1,5 +1,4 @@
 def is_year_leap(year):
-
     if year % 4 == 0:
         return True
     else:
@@ -7,7 +6,5 @@ def is_year_leap(year):
 
 
 check_year = 2024
-
 result = is_year_leap(check_year)
-
 print(f"год {check_year}: {result}")
